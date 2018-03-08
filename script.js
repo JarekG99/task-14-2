dda7f20f8bee220003711637903517ad8115c7f2
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-03-08 09:13:44 +0100

    Add object of html attr.

--- script.js
+++ script.js
@@ -23,19 +23,19 @@
       id: 1,
       title: 'Harry Potter',
       desc: 'Film o czarodzieju.',
-      poster: ''
+      poster: 'photos/harrypotter.jpeg'
     },
     {
       id: 2,
       title: 'Król Lew',
       desc: 'Film o lwie - królu sawanny.',
-      poster: ''
+      poster: 'photos/king_lion.jpeg'
     },
     {
       id: 3,
       title: 'Czterysta batów',
       desc: 'Moving story of a young boy who, left without attention, delves into a life of petty crime.',
-      poster: '<img src={ require("./photos/czterysta_batow.jpg") } alt="cos"/>'
+      poster: 'photos/czterysta_batow.jpg'
     }
   ];
 
@@ -43,7 +43,7 @@ var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
       React.createElement('p', {}, movie.desc),
-      React.createElement('div', {}, movie.poster)
+      React.createElement('img', {src: movie.poster})
     );
 });
 
